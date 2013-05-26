5237bb7c410836d4f2f6ce51d278558295d2420d
Luis Francisco Guerrero <luis.f.guerrero@gmail.com>
2013-05-26 14:46:43 -0700

    update application layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
 
   def index
     @owner = Owner.new
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
   end
 
+end
+flash.each do |key, value|
+  puts '<div class="' + key + '">' + value + '</div>'
 end
